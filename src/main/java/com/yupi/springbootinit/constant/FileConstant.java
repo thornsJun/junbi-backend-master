2b774b131f63c108b9d192295da35deb2a29a96f
Thorns19 <1592226841@qq.com>
2023-07-26 16:22:43 +0800

    Initial commit

---
+++ src/main/java/com/yupi/springbootinit/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.yupi.springbootinit.constant;
+
+/**
+ * 文件常量
+ *  @author thorns
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://yupi.icu";
+}
